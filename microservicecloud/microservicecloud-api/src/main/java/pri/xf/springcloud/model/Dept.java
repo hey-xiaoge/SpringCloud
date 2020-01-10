b7e13467b4741091f7bcdcaa785a5b839c1569e7
hey-xiaoge <731414559@qq.com>
2020-01-10 17:29:58 +0800

    第一次提交，刚建立好子服务并未测试

---
+++ microservicecloud/microservicecloud-api/src/main/java/pri/xf/springcloud/model/Dept.java
@@ -0,0 +1,22 @@
+package pri.xf.springcloud.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+
+@AllArgsConstructor //全参构造
+@NoArgsConstructor //无构造
+@Data
+@Accessors(chain=true)
+public class Dept implements Serializable { //1.必须序列化  2.Dept orm mysql->Dept(table)类关系映射
+
+    private Long id; //主键
+    private String dname; //部门名称
+    private String db_source;//来自哪个数据库，因为微服务架构可以一个服务对要一个数据库，同一个信息被存储到不同数据库
+
+
+
+}
---
+++ microservicecloud/src/test/java/pri/xf/springcloud/AppTest.java
@@ -0,0 +1,20 @@
+package pri.xf.springcloud;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
