d915d48180829390c816f4e76277e2d9cf42f215
hey-xiaoge <731414559@qq.com>
2020-01-14 17:23:16 +0800

    构建了一个完整的SpringCloud案例

---
+++ microservicecloud/microservicecloud-provider-dept-8001/src/main/java/pri/xf/springcloud/service/DeptService.java
@@ -0,0 +1,13 @@
+package pri.xf.springcloud.service;
+
+import java.util.List;
+import pri.xf.springcloud.model.Dept;
+
+
+public interface DeptService {
+    public boolean add(Dept dept);
+
+    public Dept get(Long id);
+
+    public List<Dept> list();
+}
---
+++ microservicecloud/microservicecloud-provider-dept-8001/src/main/java/pri/xf/springcloud/service/impl/DeptServiceImpl.java
@@ -0,0 +1,36 @@
+package pri.xf.springcloud.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import pri.xf.springcloud.dao.DeptDao;
+import pri.xf.springcloud.model.Dept;
+import pri.xf.springcloud.service.DeptService;
+
+@Service
+public class DeptServiceImpl implements DeptService {
+
+    @Autowired
+    private DeptDao deptDao;
+
+    @Override
+    public boolean add(Dept dept) {
+        return deptDao.addDept(dept);
+    }
+
+
+    @Override
+    public Dept get(Long id) {
+        return deptDao.findById(id);
+    }
+
+
+    @Override
+    public List<Dept> list() {
+        return deptDao.findAll();
+    }
+
+
+}
